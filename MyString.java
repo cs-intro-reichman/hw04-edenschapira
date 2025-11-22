fe7e1bf32eb40c4f85f3ffb0dfff7df1839e4c5d
Eden Schapira <eden.schapira@post.runi.ac.il>
2025-11-22 12:19:59 +0200

    MyString test1

--- MyString.java
+++ MyString.java
@@ -1,10 +1,10 @@
 public class MyString {
     public static void main(String[] args) {
-        System.out.println("Testing lowercase:");
-        System.out.println("UnHappy : " + lowerCase("UnHappy"));
-        System.out.println("This costs 15 Sheksls : " + lowerCase("This costs 15 Sheksls"));
-        System.out.println("TLV : " + lowerCase("TLV"));
-        System.out.println("lowercase : " + lowerCase("lowercase"));
+        // System.out.println("Testing lowercase:");
+        // System.out.println("UnHappy : " + lowerCase("UnHappy"));
+        // System.out.println("This costs 15 Sheksls : " + lowerCase("This costs 15 Sheksls"));
+        // System.out.println("TLV : " + lowerCase("TLV"));
+        // System.out.println("lowercase : " + lowerCase("lowercase"));
 
         System.out.println("Testing contains:");
         System.out.println(contains("unhappy", "happy")); // true
@@ -18,13 +18,29 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        String newStr = "";
+        for(int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+            if(ch >= 'A' && ch <= 'Z'){
+              newStr += (char) (ch + 32); 
+            } else {
+              newStr += ch; 
+            }
+        }
+        return newStr;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
-        return false;
+        if(str1.length() < str2.length()){
+            return false;
+        }
+        for (int i = 0; i < str2.length(); i++){
+            if (str1.indexOf(str2.charAt(i)) == -1) {
+                return false;          
+            }
+        }
+        return true;
     }
+
 }
