c112f94394ec7d605a357d62813f4bb911d1f50b
Eden Schapira <eden.schapira@post.runi.ac.il>
2025-11-22 09:30:52 +0200

    arrcharops test 5

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -163,7 +163,7 @@ public class ArrCharOps {
      *         return -2 if there is an error with the input.
      */
     public static int compareTo(String str1, String str2) {
-        if (str1 == null || str2 == null) {
+        if (str1 == null || str2 == null || str1.length() == 0 || str2.length() == 0) {
         return -2;
         }
     int length1 = str1.length();
